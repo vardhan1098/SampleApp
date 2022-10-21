f51e7364414bf9681c6951012efa795ded98dc48
Sasi Reddy <damm7371098@gmail.com>
2022-10-21 16:05:38 +0530

    new app

--- Src/Screens/Data.Screen.tsx
+++ Src/Screens/Data.Screen.tsx
@@ -1,96 +1,208 @@
-import React, { FC, useEffect, useState } from "react"
-import { Alert, FlatList, KeyboardAvoidingView, Button,Platform, SafeAreaView, ScrollView, StyleSheet, Text, TouchableOpacity, View } from "react-native"
+// import React, { FC, useEffect, useState } from "react"
+// import { Alert, FlatList, KeyboardAvoidingView, Button,Platform, SafeAreaView, ScrollView, StyleSheet, Text, TouchableOpacity, View } from "react-native"
+
+// import AsyncStorage from '@react-native-async-storage/async-storage';
+
+
+
+// const HomeScreen = ({navigation}: {navigation: any}) => {
+//   const [propsdata, setprops] = useState()
+//   const [ListData, SetListData] = useState([])
+//   useEffect(() => {
+//     AsyncData()
+//     // renderdata()
+//     console.log("for ned")
+//     LoadData()
+//   }, [])
+
+//   const AsyncData = async () => {
+//     let data = await AsyncStorage.getItem("user_data");
+//     //   console.log("data",data)
+//   }
+
+//   const LoadData = async () => {
+//     console.log("...first step")
+//     var config = {
+//       method: 'get',
+//       url: 'https://jsonplaceholder.typicode.com/posts',
+//     };
+
+//     try{
+//     let response=await fetch(config.url)
+//     let data=await response.json()
+//     SetListData(data)
+//     }catch(err){
+//       console.log("err in fetch ",err)
+//     }
+     
+
+//   }
+
+
+
+//   const renderdata = (data: object) => {
+//     console.log("====hosakda",data.item)
+//     return (
+//     //   <Card
+//     //     numberOfLines={true}
+//     //     propsdata={data.item}
+//     //     onpress={() => { props.navigation.navigate("PostDatilScreen", { data: data.item }) }}
+//     //   ></Card>
+//     <View style={{flex:1,flexDirection:'row', marginRight: 19, marginLeft: 15, marginTop: 10, backgroundColor:'#DFE4EE',padding: 15,borderRadius: 7,}}>
+//         <Text>{data.item.id} .</Text>
+//         <TouchableOpacity  onPress={() => navigation.navigate('DetailsDataScreen')}>
+//         <Text style={styles.theader}>{data.item.title}</Text>
+//         </TouchableOpacity>
+//     </View>
+//     )
+//   }
+
+//   const FoterStyle = () => {
+//     return <View style={{ height: 50, width: "100%" }}></View>
+//   }
+//   return (
+//     <SafeAreaView style={styles.MainContainer}>
+//       <View style={styles.container}>
+       
+//         <FlatList
+//           data={ListData}
+//           keyExtractor={(item, index) => index.toString()}
+//           renderItem={renderdata}
+//           ListFooterComponent={FoterStyle}
+//         ></FlatList>
+     
+//       </View>
+//     </SafeAreaView>
+//   )
+// }
+// export default HomeScreen;
+// const styles = StyleSheet.create({
+//   MainContainer: {
+//     flex: 1,
+//     backgroundColor: "white",
+//   },
+//   container: {
+//     height: "100%",
+//     width: "100%",
+//     backgroundColor: "white",
+//     alignItems: "center",
+//     justifyContent: "center"
+//   },
+//   theader:{
+//     paddingLeft:5,
+//     color:"darkblue"
+//   }
+// })
+import React, {FC, useEffect, useState} from 'react';
+import {
+  Alert,
+  FlatList,
+  KeyboardAvoidingView,
+  Button,
+  Platform,
+  SafeAreaView,
+  ScrollView,
+  StyleSheet,
+  Text,
+  TouchableOpacity,
+  View,
+} from 'react-native';
 
 import AsyncStorage from '@react-native-async-storage/async-storage';
 
-
-
 const HomeScreen = ({navigation}: {navigation: any}) => {
-  const [propsdata, setprops] = useState()
-  const [ListData, SetListData] = useState([])
+  const [propsdata, setprops] = useState();
+  const [ListData, SetListData] = useState([]);
   useEffect(() => {
-    AsyncData()
+    AsyncData();
     // renderdata()
-    console.log("for ned")
-    LoadData()
-  }, [])
+    console.log('for ned');
+    LoadData();
+  }, []);
 
   const AsyncData = async () => {
-    let data = await AsyncStorage.getItem("user_data");
+    let data = await AsyncStorage.getItem('user_data');
     //   console.log("data",data)
-  }
+  };
 
   const LoadData = async () => {
-    console.log("...first step")
+    console.log('...first step');
     var config = {
       method: 'get',
       url: 'https://jsonplaceholder.typicode.com/posts',
     };
 
-    try{
-    let response=await fetch(config.url)
-    let data=await response.json()
-    SetListData(data)
-    }catch(err){
-      console.log("err in fetch ",err)
+    try {
+      let response = await fetch(config.url);
+      let data = await response.json();
+      SetListData(data);
+    } catch (err) {
+      console.log('err in fetch ', err);
     }
-     
-
-  }
-
-
+  };
 
   const renderdata = (data: object) => {
-    console.log("====hosakda",data.item)
+    console.log('====hosakda', data.item);
     return (
-    //   <Card
-    //     numberOfLines={true}
-    //     propsdata={data.item}
-    //     onpress={() => { props.navigation.navigate("PostDatilScreen", { data: data.item }) }}
-    //   ></Card>
-    <View style={{flex:1,flexDirection:'row', marginRight: 19, marginLeft: 15, marginTop: 10, backgroundColor:'#DFE4EE',padding: 15,borderRadius: 7,}}>
+      //   <Card
+      //     numberOfLines={true}
+      //     propsdata={data.item}
+      //     onpress={() => { props.navigation.navigate("PostDatilScreen", { data: data.item }) }}
+      //   ></Card>
+      <View
+        style={{
+          flex: 1,
+          flexDirection: 'row',
+          marginRight: 19,
+          marginLeft: 15,
+          marginTop: 10,
+          backgroundColor: '#DFE4EE',
+          padding: 15,
+          borderRadius: 7,
+        }}>
         <Text>{data.item.id} .</Text>
-        <TouchableOpacity  onPress={() => navigation.navigate('DetailsDataScreen')}>
-        <Text style={styles.theader}>{data.item.title}</Text>
+        <TouchableOpacity
+          onPress={() =>
+            navigation.navigate('DetailsDataScreen', {post: data.item})
+          }>
+          <Text style={styles.theader}>{data.item.title}</Text>
         </TouchableOpacity>
-    </View>
-    )
-  }
+      </View>
+    );
+  };
 
   const FoterStyle = () => {
-    return <View style={{ height: 50, width: "100%" }}></View>
-  }
+    return <View style={{height: 50, width: '100%'}}></View>;
+  };
   return (
     <SafeAreaView style={styles.MainContainer}>
       <View style={styles.container}>
-       
         <FlatList
           data={ListData}
           keyExtractor={(item, index) => index.toString()}
           renderItem={renderdata}
-          ListFooterComponent={FoterStyle}
-        ></FlatList>
-     
+          ListFooterComponent={FoterStyle}></FlatList>
       </View>
     </SafeAreaView>
-  )
-}
+  );
+};
 export default HomeScreen;
 const styles = StyleSheet.create({
   MainContainer: {
     flex: 1,
-    backgroundColor: "white",
+    backgroundColor: 'white',
   },
   container: {
-    height: "100%",
-    width: "100%",
-    backgroundColor: "white",
-    alignItems: "center",
-    justifyContent: "center"
+    height: '100%',
+    width: '100%',
+    backgroundColor: 'white',
+    alignItems: 'center',
+    justifyContent: 'center',
   },
-  theader:{
-    paddingLeft:5,
-    color:"darkblue"
-  }
-})
+  theader: {
+    paddingLeft: 5,
+    color: 'darkblue',
+  },
+});
+
 
--- Src/Screens/DetailsData.Screen.tsx
+++ Src/Screens/DetailsData.Screen.tsx
@@ -1,24 +1,91 @@
 //import liraries
-import React, { Component } from 'react';
-import { View, Text, StyleSheet } from 'react-native';
+import {getDefaultNormalizer} from '@testing-library/react-native';
+import React, {Component, useEffect, useState} from 'react';
+import {View, Text, StyleSheet, FlatList} from 'react-native';
+interface comment {
+  postId: number;
+  id: number;
+  name: string;
+  email: string;
+  body: string;
+}
 
 // create a component
-const MyComponent = () => {
+const MyComponent = ({route}) => {
+  const [comments, setComments] = useState<Array<comment>>([]);
+  const getPostComments = async (id: number) => {
+    console.log(id);
+    try {
+      let response = await fetch(
+        `https://jsonplaceholder.typicode.com/posts/${id}/comments`,
+      );
+      let data = await response.json();
+      console.log(data);
+      setComments(data);
+    } catch (err) {
+      console.log(err);
+    }
+  };
+  useEffect(() => {
+    console.log(route.params.post.id);
+    getPostComments(route.params.post.id);
+  }, []);
+  const renderItem = ({item, index}) => {
     return (
-        <View style={styles.container}>
-            <Text>MyComponent</Text>
-        </View>
+      <View style={styles.post}>
+        <Text style={{fontSize: 16, fontWeight: '500'}}>{item.email}</Text>
+        <Text style={{fontSize: 13, fontWeight: '500'}}>{item.name}</Text>
+
+        <Text style={{fontSize: 13, fontWeight: '500'}}>{item.body}</Text>
+      </View>
     );
+  };
+
+  return (
+    <View style={styles.container}>
+      <View style={styles.post}>
+        <Text style={styles.title}>{route.params.post.title} </Text>
+        <Text style={styles.title}>{route.params.post.body}</Text>
+      </View>
+      <Text style={{color: 'cyan', marginHorizontal: 20}}>Comments:</Text>
+      <FlatList
+        testID="COMMENT_LIST"
+        data={comments}
+        keyExtractor={item => item.id.toString()}
+        renderItem={renderItem}
+      />
+    </View>
+  );
 };
 
 // define your styles
 const styles = StyleSheet.create({
-    container: {
-        flex: 1,
-        justifyContent: 'center',
-        alignItems: 'center',
-        backgroundColor: '#2c3e50',
-    },
+  container: {
+    flex: 1,
+
+    // alignItems: 'center',
+    backgroundColor: '#2c3e50',
+  },
+  post: {
+    elevation: 5,
+    borderRadius: 10,
+    backgroundColor: 'white',
+    color:"blue",
+    width: '95%',
+    padding: 10,
+    marginVertical: 20,
+    alignSelf: 'center',
+    paddingHorizontal: 20,
+  },
+  title: {
+    fontSize: 15,
+    color:"indigo",
+    fontWeight: '600',
+    marginVertical: 10,
+  },
+  new:{
+    color:"deepskyblue"
+  }
 });
 
 //make this component available to the app
